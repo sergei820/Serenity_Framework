91c51cfcaed86c4460b821f7e8689758feb0fb2e
Sergei Kamendrovskii <Sergei.Kamendrovskii@externals.adidas.com>
2022-08-06 16:32:35 +0400

    Slack integration - working

---
+++ src/main/java/integrations/SlackIntegration.java
@@ -0,0 +1,31 @@
+package integrations;
+
+import com.github.seratch.jslack.Slack;
+import com.github.seratch.jslack.api.webhook.Payload;
+import com.github.seratch.jslack.api.webhook.WebhookResponse;
+
+import java.io.IOException;
+
+public class SlackIntegration {
+
+    private final static String WEB_HOOK_URL = "https://hooks.slack.com/services/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";
+    private final static String O_AUTH_TOKEN = "xoxb-3905122468548-3905202928052-T0FuEciaynxxoBn5LcR2TJxd";
+    private final static String SLACK_CHANNEL = "at-mentoring-intermediate";
+
+    public static void main(String[] args) {
+        sendMessageToSlack("Test message to Slack");
+    }
+
+    public static void sendMessageToSlack(String message) {
+        try {
+            StringBuilder msgBuilder = new StringBuilder();
+            msgBuilder.append(message);
+
+            Payload payload = Payload.builder().channel(SLACK_CHANNEL).text(msgBuilder.toString()).build();
+            WebhookResponse webhookResponse = Slack.getInstance().send(WEB_HOOK_URL, payload);
+            webhookResponse.getMessage();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
