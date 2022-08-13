a7d62c7223c9a1412672cf57500f71e4c3d7a2f3
Sergei Kamendrovskii <Sergei.Kamendrovskii@externals.adidas.com>
2022-08-13 19:29:57 +0400

    TestRail integration recovering framework

--- src/main/java/integrations/testrail/TestRailIntegration.java
+++ src/main/java/integrations/testrail/TestRailIntegration.java
@@ -1,19 +1,10 @@
 package integrations.testrail;
 
 import com.codepine.api.testrail.TestRail;
-import lombok.Getter;
 
 public class TestRailIntegration {
 
-    static TestRail testRail;
-    private final static String endPoint = "http://testrail.company.com";
-    private final static String username = "username";
-    private final static String password = "password";
-
-    public static TestRail  createTestRailInstance() {
-        if (testRail == null) {
-            testRail = TestRail.builder(endPoint, username, password).build();
-        }
-        return testRail;
-    }
+    //APIClient client = new APIClient("https://serenityframework.testrail.io/");
+    //client.setUser(PropertiesLoader.loadProperty("testRailUser"));
+    //client.setPassword(PropertiesLoader.loadProperty("testRailPassword"));
 }
